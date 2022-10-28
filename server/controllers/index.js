4f59530b5d6ea3f0d8567d7caaae38e4bebd466d
Benton Le <ble21@my.centennialcollege.ca>
2022-10-27 21:38:51 -0400

    Finished Routing and Button Functionality

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,3 @@
 export function DisplayHomePage(req, res, next) {
     res.render('index', { title: 'Home', page: 'home' });
 }
-export function DisplayBookListPage(req, res, next) {
-    res.render('index', { title: 'List', page: 'list' });
-}
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,12 @@ import { Router } from 'express';
 
 let router = Router();
 
-import {DisplayHomePage, DisplayBookListPage} from '../controllers/index.js';
+import {DisplayHomePage} from '../controllers/index.js';
 
 /* Display home page. */
 router.get('/', DisplayHomePage);
 
 /* Display home page. */
 router.get('/home', DisplayHomePage);
-
-router.get('/list', DisplayBookListPage);
 
 export default router;
